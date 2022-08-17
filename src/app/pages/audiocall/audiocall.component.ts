ced2dd0ddfb5daa74a743727a8baf67007de1596
mDjeee <isejtniyazov@mail.ru>
2022-08-17 12:26:33 +0500

    feat: setup basic angular

---
+++ src/app/pages/audiocall/audiocall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audiocall',
+  templateUrl: './audiocall.component.html',
+  styleUrls: ['./audiocall.component.css']
+})
+export class AudiocallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
