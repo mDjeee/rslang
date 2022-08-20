613db74da30a68a54caa6773ccb2b1a536a89d67
mDjeee <isejtniyazov@mail.ru>
2022-08-20 23:42:35 +0500

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/header/header-components/navigation/navigation.component';
+import { LogoComponent } from './components/header/header-components/logo/logo.component';
+import { AuthorizationBtnComponent } from './components/header/header-components/authorization-btn/authorization-btn.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { CapabilitiesComponent } from './components/capabilities/capabilities.component';
+import { TeamComponent } from './components/team/team.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShareModule } from './share/share.module';
 import { CoreModule } from './core/core.module';
 
@@ -12,6 +21,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule
     ShareModule,
     CoreModule
   ],
