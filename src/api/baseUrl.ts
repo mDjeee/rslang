467bdd4a6e387acdddbaa52a89d565f0dafdeb26
mDjeee <isejtniyazov@mail.ru>
2022-08-20 23:39:09 +0500

    feat: add baseWord api url

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,1 +1,3 @@
 export const baseUrl = 'https://angular-learndwords.herokuapp.com';
+export const baseWordsUrl = 'https://angular-learndwords.herokuapp.com/words';
+export const baseUsersUrl = 'https://angular-learndwords.herokuapp.com/users';
