7eb81b1c5f61f704c242f4e943566803b8809d18
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-08-20 12:27:29 +0500

    refactor: move application logo component from header subcomponents to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/header/header-components/navigation/navigation.component';
-import { LogoComponent } from './components/header/header-components/logo/logo.component';
+import { AppLogoComponent } from './components/appLogo/appLogo.component';
 import { AuthorizationBtnComponent } from './components/header/header-components/authorization-btn/authorization-btn.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './pages/main/main.component';
@@ -21,7 +21,7 @@ import { BookComponent } from './pages/book/book.component';
     HeaderComponent,
     BookComponent,
     NavigationComponent,
-    LogoComponent,
+    AppLogoComponent,
     AuthorizationBtnComponent,
     MainComponent,
     CapabilitiesComponent,
---
+++ src/app/components/appLogo/appLogo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appLogo',
+  templateUrl: './appLogo.component.html',
+  styleUrls: ['./appLogo.component.css']
+})
+export class AppLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
