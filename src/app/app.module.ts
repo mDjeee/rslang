751c201cbc38c3d78b6c937817f70b8aadaabbe3
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-08-20 10:43:40 +0500

    refactor: remove unsused declarations in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BookComponent } from './pages/book/book.component';
-import { SprintComponent } from './pages/sprint/sprint.component';
-import { AudiocallComponent } from './pages/audiocall/audiocall.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/header/header-components/navigation/navigation.component';
 import { LogoComponent } from './components/header/header-components/logo/logo.component';
@@ -17,9 +14,6 @@ import { TeamComponent } from './components/team/team.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
-    SprintComponent,
-    AudiocallComponent,
     HeaderComponent,
     NavigationComponent,
     LogoComponent,
