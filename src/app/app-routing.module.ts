f5bdd2734ebfbd2d1d263bf5f0b8de5472914579
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-08-23 12:55:34 +0500

    feat: add path for level component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { BookComponent } from './pages/book/book.component';
 import { AudiocallComponent } from './pages/audiocall/audiocall.component';
 import { SprintComponent } from './pages/sprint/sprint.component';
 import { MainComponent } from './pages/main/main.component';
+import {LevelComponent} from "./pages/sprint/level/level.component";
 
 const appRoutes: Routes = [
   { path: '', component: MainComponent },
   { path: 'textbook', component: BookComponent },
   { path: 'games/audiocall', component: AudiocallComponent },
   { path: 'games/sprint', component: SprintComponent },
+  { path: 'games/sprint/level', component: LevelComponent}
   // { path: 'authorization', component: NotFoundPage },
   // { path: '**', component: NotFoundComponent },
 ]
