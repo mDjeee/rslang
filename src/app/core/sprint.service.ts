d91a6cc0fa701e763380fcc05ed21c8fbb6c0071
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-09-02 20:27:59 +0500

    feat: add music

--- src/app/core/sprint.service.ts
+++ src/app/core/sprint.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Subscription, timer} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SprintService {
-
+  seconds!:number | string
+  public subscription!: Subscription;
+  audio = new Audio();
   constructor() { }
+
+  public playSong(): void {
+    this.audio.src = '../../assets/music/single.mp3';
+    this.audio.load();
+    this.audio.play();
+  }
+  public stopSong() {
+    this.audio.pause();
+  }
+
+
 }
